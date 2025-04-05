b4d456ed1b05d5c2784f16c170101a484dd5d5ac
spartan0000 <doctor.dlee@gmail.com>
2025-04-05 03:36:00 -0500

    new file

---
+++ researcher.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+from typing import TypedDict, List
+from langgraph.graph import StateGraph, END
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage
+
+
+load_dotenv()
+
+llm = ChatOpenAI(model = "gpt-3.5-turbo", temperature = 0.2)
+
+class State(TypedDict):
+    text: str
+    articles: List[str]
+    summary: str
