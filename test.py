0c39a148ad2e515b388045aa7ab2b0e3a7743b54
spartan0000 <doctor.dlee@gmail.com>
2025-04-05 03:33:56 -0500

    progress

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
 #test file
+
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+from typing import TypedDict, List
+from langgraph.graph import StateGraph, END
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage
+
+
+load_dotenv()
+
+llm = ChatOpenAI(model = "gpt-3.5-turbo", temperature = 0.2)
+
+response = llm.invoke("hello, what is the capital of France?")
+
+print(response.content)
+
+class State(TypedDict):
+    text: str
+    articles: List[str]
+    summary: str
+
+
+    
+
+
+
