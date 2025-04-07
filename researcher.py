f0a8a656c1802ed67c468a2fd1049180b39fcd15
spartan0000 <doctor.dlee@gmail.com>
2025-04-06 21:17:25 -0500

    function calling openAI

--- researcher.py
+++ researcher.py
@@ -14,6 +14,8 @@ from Bio import Entrez
 
 from openai import OpenAI
 
+import json
+
 #load functions from abstract.py
 from abstract import get_articles, make_query, create_string, convert_query
 
@@ -25,6 +27,7 @@ llm = ChatOpenAI(model = "gpt-3.5-turbo", temperature = 0.2)
 
 
 class State(TypedDict):
+    user_input: str
     query: str
     start_date: str
     end_date: str
@@ -34,7 +37,48 @@ class State(TypedDict):
 
 #need a way to get free text query into the format we want it to query pubmed
 
+def format_query(state: State) -> State:
+    """
+    take the user input query and return a dictionary that contains the query, start date, end date, and number of articles
+    """
+    system_prompt = """Extract the search query terms of interest, the start date, end date, and number of articles required.
+    
+    """
 
+    input_text = state['user_input']
+    result = llm.invoke(
+        messages = [
+            {'role':'system', 'content': system_prompt},
+            {'role':'user', 'content': input_text},
+        ],
+        functions = [
+            {
+                "name": "generate_query_parameters",
+                "description": "Extracts query terms, dates, and number of articles",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {"type":"string"},
+                        "start_date": {"type":"string"},
+                        "end_date": {"type":"string"},
+                        "num_articles": {"type":"integer"},
+                    },
+                    "required": ["query", "start_date", "end_date", "num_articles"]
+                }
+            }
+        ],
+        function_call = {"name": "generate_query_parameters"}
+    )
+
+    args = result.additional_kwargs['function_call']['arguments']
+    parsed = json.loads(args)
+
+    return {
+        "query":parsed["query"],
+        "start_date":parsed["start_date"],
+        "end_date":parsed["end_date"],
+        "num_articles":parsed["num_articles"],
+    }
 
 
 
@@ -47,7 +91,12 @@ def get_article_node(state: State) -> State:
     Get the abstract from pubmed and add it to the state
     """
     #print('input state:', state)
-    queries = make_query(state['query'], state['start_date'], state['end_date'])
+    date_range = f'("{state['start_date']}"[Date - Publication] : "{state['end_date']}"[Date - Publication])'
+    
+    queries = f'{state['query']}[Title/Abstract] AND {date_range}'
+
+
+
     #print('queries:', queries)
     articles = get_articles(queries, state['num_articles'])
     abstract_text = create_string(articles)
@@ -80,11 +129,14 @@ def summarize_node(state: State) -> State:
 
 builder = StateGraph(State)
 
+builder.add_node("format_query", format_query)
 builder.add_node("get_abstracts", get_article_node)
 builder.add_node("summarize", summarize_node)
 
-builder.set_entry_point("get_abstracts")
+builder.set_entry_point("format_query")
+builder.add_edge("format_query", "get_abstracts")
 builder.add_edge("get_abstracts", "summarize")
+
 
 graph = builder.compile()
 
