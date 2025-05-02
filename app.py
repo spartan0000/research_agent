4c19e4e58eda4a4934d0a89a6e1244a0ea283db8
spartan0000 <doctor.dlee@gmail.com>
2025-05-02 02:21:35 -0500

    app runs

--- app.py
+++ app.py
@@ -2,4 +2,15 @@ import streamlit as st
 
 from pubmed import run_pubmed
 
-st.title("Pubmed Summarizer")
+st.title("Pubmed Query and Summary")
+
+query = st.text_input("Enter your pubmed query topic and dates if relevant: ")
+
+if st.button("Run"):
+    with st.spinner('Running...'):
+        initial_state = {'user_input': query}
+
+        result = run_pubmed(initial_state)
+        st.write("Here's what I found based on your query: ")
+        st.write(result.get("summary", "No summary available"))
+
